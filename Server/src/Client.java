c07a3d375e35a6a1588146e64685e06208e374ab
Natalia Dombrowska <dombrowskanatalia@gmail.com>
2019-10-17 14:18:44 +0100

    Uppercase String sent back to client

--- Server/src/Client.java
+++ Server/src/Client.java
@@ -1,7 +1,12 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 public class Client {
@@ -9,14 +14,16 @@ public class Client {
 	public static void main(String[] args) {
 		try {
 			Socket socket = new Socket("localhost", 23456);
-			
-			OutputStream os = socket.getOutputStream();
-			PrintWriter pw = new PrintWriter(os); 
-			pw.print("hello world");
-			
-			pw.close();
-			socket.close();
 
+			PrintWriter pw = new PrintWriter(socket.getOutputStream()); 
+			pw.println("This is your message back in capital letters");
+			pw.flush();
+			
+			InputStreamReader in = new InputStreamReader(socket.getInputStream());
+			BufferedReader bf = new BufferedReader(in);
+			
+			String str = bf.readLine();
+			System.out.println("server:" + str);
 			
 		}catch (IOException e) {
 			System.err.println("this is the client error");
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,7 +1,12 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Server {
@@ -10,21 +15,26 @@ public class Server {
 		ServerSocket serverSocket = new ServerSocket(23456);
 		Socket connectionSocket = serverSocket.accept(); // wait for a connection
 		
-		InputStream is = connectionSocket.getInputStream();
-		Scanner sc = new Scanner(is);
+		System.out.println("client connected");
 		
-		System.out.println(sc.nextLine());
-		sc.close();
+		InputStreamReader in = new InputStreamReader(connectionSocket.getInputStream());
+		BufferedReader bf = new BufferedReader(in);
+		
+		String str = bf.readLine();
+		System.out.println("client:" + str);
+		
+		PrintWriter pw = new PrintWriter(connectionSocket.getOutputStream());
+		pw.println(str.toUpperCase());
+		pw.flush();
+		
+		
+		
 		
 		serverSocket.close();
 		} catch (IOException e) {
 			System.err.println("error");
 		}
-		
 	}
 	
-	public String getCapitals() {
-		return "";
-	}
 }
 
