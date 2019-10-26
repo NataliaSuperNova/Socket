b19c4599b2ffdc8e5f5f7ecff9872e9c0110c2f3
Lwallace3 <45268305+Lwallace3@users.noreply.github.com>
2019-10-26 11:15:52 +0100

    Update Server.java

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -10,30 +10,29 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Server {
-	public static void main(String[] args) throws IOException {
-		ServerSocket serverSocket = new ServerSocket(23456);
-		
-		while (true) {
-			Socket connectionSocket = serverSocket.accept(); // wait for a connection
-			
-			System.out.println("client connected");
-			
-			InputStreamReader in = new InputStreamReader(connectionSocket.getInputStream());
-			BufferedReader bf = new BufferedReader(in);
-			
-			
-			String str = bf.readLine();
-			System.out.println("client:" + str);
-			
-			PrintWriter pw = new PrintWriter(connectionSocket.getOutputStream());
-			pw.println(str.toUpperCase());
-			pw.flush();
-			
-			int clientNum = bf.read();	
-			System.out.println(clientNum);
-			pw.println(clientNum*2);
-			pw.flush();
-		}	
-	}
-}
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(24356);
 
+        while (true) {
+            Socket connectionSocket = serverSocket.accept(); // wait for a connection
+
+            System.out.println("client connected");
+
+            InputStreamReader in = new InputStreamReader(connectionSocket.getInputStream());
+            BufferedReader bf = new BufferedReader(in);
+
+
+            String str = bf.readLine();
+            System.out.println("client:" + str);
+
+            PrintWriter pw = new PrintWriter(connectionSocket.getOutputStream());
+            pw.println(str.toUpperCase());
+            pw.flush();
+
+            int clientNum = bf.read();
+            System.out.println(clientNum);
+            pw.println(clientNum*2);
+            pw.flush();
+        }
+    }
+}
